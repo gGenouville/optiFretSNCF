25e854d6853d518a4ee7ba19af78985ee80788e6
alexitemoi <johndoe@example.com>
2025-02-14 15:12:45 +0100

    folders

---
+++ code/module/code.py
@@ -0,0 +1,27 @@
+"""
+module description
+"""
+
+import pandas as pd 
+
+
+def data_open(path):
+    """
+    Reads an Excel file from the specified path and 
+    returns sheet names and their corresponding DataFrames.
+
+    Args:
+        path (str): Path to the Excel file.
+
+    Returns:
+        tuple: A tuple containing two lists:
+            - List of sheet names.
+            - List of DataFrames corresponding to each sheet.
+    """
+    all_sheets = pd.read_excel(path, sheet_name=None)
+    results = ([],[])
+    for sheet_name, df in all_sheets.items():
+        results[0].append(sheet_name)
+        results[1].append(df)
+
+    return results

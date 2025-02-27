3acf87ed52d0cc60086bcc5749b945ecc3ab3498
Lacroix Imanol <imanol.lacroix@student-cs.fr>
2025-02-27 10:56:00 +0100

    Modif dictionnaire D

--- code/module/utils.py
+++ code/module/utils.py
@@ -4,6 +4,7 @@ module docstring
 
 import pandas as pd
 
+
 class Feuille:
     """
     Class to store the names of the shits.
@@ -16,7 +17,8 @@ class Feuille:
     SILLON_DEPART = 'Sillons depart'
     ROULEMENTS = 'Roulements agents'
 
-class Colonne :
+
+class Colonne:
     """
     Class to store the names of the columns in a df
     """
@@ -28,7 +30,8 @@ class Colonne :
     DATE_DEPART = "Jour depart"
     ID_WAGON = "Id wagon"
 
-def data_open(path:str)->dict[str,pd.DataFrame]:
+
+def data_open(path: str) -> dict[str, pd.DataFrame]:
     """
     Reads an Excel file from the specified path and 
     returns a dictionary of sheet names and their 
@@ -47,29 +50,33 @@ def data_open(path:str)->dict[str,pd.DataFrame]:
         results[sheet_name] = df
     return results
 
+
 def get_link_id(arrival_train_id, departure_train_id):
     """
     creates an id for a link
     """
     return f"{arrival_train_id} {departure_train_id}"
 
-def init_dict_correspondance(df_correspondance : pd.DataFrame)-> dict:
-    input_df=df_correspondance.astype(str)
+
+def init_dict_correspondance(df_correspondance: pd.DataFrame) -> dict:
+    input_df = df_correspondance.astype(str)
 
     input_df[Colonne.ID_TRAIN_ARRIVEE] = (
         input_df[Colonne.N_TRAIN_ARRIVEE]
         + "_"
-        + pd.to_datetime(input_df[Colonne.DATE_ARRIVEE], format="%d/%m/%Y", errors="coerce").dt.strftime('%d')
+        + pd.to_datetime(input_df[Colonne.DATE_ARRIVEE],
+                         format="%d/%m/%Y", errors="coerce").dt.strftime('%d')
     )
 
     input_df[Colonne.ID_TRAIN_DEPART] = (
-            input_df[Colonne.N_TRAIN_DEPART]
-            + "_"
-            + pd.to_datetime(input_df[Colonne.DATE_DEPART], format="%d/%m/%Y", errors="coerce").dt.strftime('%d')
+        input_df[Colonne.N_TRAIN_DEPART]
+        + "_"
+        + pd.to_datetime(input_df[Colonne.DATE_DEPART],
+                         format="%d/%m/%Y", errors="coerce").dt.strftime('%d')
     )
-    d={}
+    d = {}
     for departure_train_id in input_df[Colonne.ID_TRAIN_DEPART]:
-        d[departure_train_id]=[]
+        d[departure_train_id] = []
     for arrival_train_id, departure_train_id in zip(
         input_df[Colonne.ID_TRAIN_ARRIVEE],
         input_df[Colonne.ID_TRAIN_DEPART],
@@ -77,12 +84,8 @@ def init_dict_correspondance(df_correspondance : pd.DataFrame)-> dict:
         d[departure_train_id].append(arrival_train_id)
     return d
 
-    
-
-
     def init_model():
         ...
 
     def init_contr():
         ...
-
